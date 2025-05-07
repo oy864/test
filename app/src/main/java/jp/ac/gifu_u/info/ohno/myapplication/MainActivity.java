18d58a1fd4f6842533758e5c31040c0967c79a94
Yuto <bigfield.0712@gmail.com>
2025-05-07 10:21:15 +0900

    Third commit

--- app/src/main/java/jp/ac/gifu_u/info/ohno/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/info/ohno/myapplication/MainActivity.java
@@ -1,36 +1,13 @@
 package jp.ac.gifu_u.info.ohno.myapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import android.widget.Button;
-import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button b = (Button) findViewById(R.id.button);
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "アプリを終了しました", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(new MyView(this));  // MyView を直接表示
     }
 }
